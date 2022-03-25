2f58bdf6f611fdd33839afc751e58b71de15b5ac
Pedro Henrique Rodrigues Da Silva <pedro_henrique.r@hotmail.com>
2022-03-24 23:07:40 -0300

    Login

--- src/components/estaticos/footer/Footer.tsx
+++ src/components/estaticos/footer/Footer.tsx
@@ -5,6 +5,7 @@ import FacebookIcon from '@material-ui/icons/Facebook';
 import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import './Footer.css';
 
 function Footer() {
   return (
@@ -12,29 +13,29 @@ function Footer() {
     
     <Grid container direction="row" justifyContent="center" alignItems="center">
                 <Grid alignItems="center" item xs={12}>
-                    <Box style={{ backgroundColor: "#3CB371", height: "120px" }}>
+                    <Box className = 'box1'>
                         <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
-                            <Typography variant="h4" align="center" gutterBottom style={{ color: "white" }}>Acompanhe:</Typography>
+                            <Typography variant="h5" align="center" gutterBottom className = 'textos'>Acompanhe:</Typography>
                         </Box>
                         <Box display="flex" alignItems="center" justifyContent="center">
                             <a href="https://github.com/pedrocadeoz" target="_blank">
-                                <GitHubIcon style={{ fontSize: 40, color: "white" }} />
+                                <GitHubIcon className = 'redes' />
                             </a>
                             <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank">
-                                <LinkedInIcon style={{ fontSize: 40, color: "white" }} />
+                                <LinkedInIcon className = 'redes' />
                             </a>
                             <a href="pedrorodrigues199767@gmail.com" target="_blank">
-                                <EmailIcon style={{ fontSize: 40, color: "white" }} />
+                                <EmailIcon className = 'redes' />
                             </a>
                         </Box>
                     </Box>
-                    <Box style={{ backgroundColor: "#3CB371", height: "60px" }}>
+                    <Box className = 'box2'>
                         <Box paddingTop={1}>
-                            <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "white" }} >© 2020 Copyright:</Typography>
+                            <Typography variant="subtitle2" align="center" gutterBottom className = 'textos' >© 2020 Copyright:</Typography>
                         </Box>
                         <Box>
                             <a target="_blank" href="https://brasil.generation.org">
-                                <Typography variant="subtitle2" gutterBottom style={{ color: "white" }} align="center">https://github.com/pedrocadeoz</Typography>
+                                <Typography variant="subtitle2" gutterBottom className = 'textos' align="center">https://github.com/pedrocadeoz</Typography>
                             </a>
                         </Box>
                     </Box>
--- src/components/estaticos/navbar/Navbar.tsx
+++ src/components/estaticos/navbar/Navbar.tsx
@@ -1,54 +1,52 @@
 import React from 'react'
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import { AppBar, Toolbar, Typography, Box } from '@material-ui/core';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 
 function Navbar() {
   return (
     <>
       <AppBar position="static">
-                <Toolbar variant="dense">
-                    <Box style={{ cursor: "pointer", backgroundColor:"#3CB371"}} >
+        <Toolbar variant="dense">
+                    <Box className = 'cursor'>
                         <Typography variant="h4" color="inherit">
                             Porco Tech
                         </Typography>
                     </Box>
 
                     <Box display="flex" justifyContent="start">
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={1} className = 'cursor'>
                             <Typography variant="h5" color="inherit">
                               Início
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={1} className = 'cursor'>
                             <Typography variant="h5" color="inherit">
                                 Postagens
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={1} className = 'cursor'>
                             <Typography variant="h5" color="inherit">
                                 Temas
                             </Typography>
                         </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                        <Box mx={1} className = 'cursor'>
                             <Typography variant="h6" color="inherit">
                                 Entre com um tema
                             </Typography>
-                        </Box>
-                        <Box mx={1} style={{ cursor: "pointer" }}>
+                </Box>
+                    <Link to='/login' className='text-decorator-none'>
+                        <Box mx={1} style={{ cursor: "pointer", color: "white"}}>
                             <Typography variant="h5" color="inherit">
                                 Sair
                             </Typography>
                         </Box>
-                    </Box>
+                    </Link>
+             </Box>
 
-                </Toolbar>
-            </AppBar>
+        </Toolbar>
+    </AppBar>
     </>
   )
 }
