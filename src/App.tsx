242c5dd7ab7c84b4264b6e9de8d92af38600ada7
Pedro Henrique Rodrigues Da Silva <pedro_henrique.r@hotmail.com>
2022-03-24 23:41:05 -0300

    Teste

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
+import Login from './paginas/login/Login'
 import './App.css';
 
 
